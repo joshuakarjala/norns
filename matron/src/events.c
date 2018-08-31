6f62a624b4cfdcd596d92b7df78c9bbb03760d05
Mark Harris <mharris@technobear.com>
2018-08-31 16:52:13 +0200

    fix rebase issue

--- matron/src/events.c
+++ matron/src/events.c
@@ -291,9 +291,6 @@ static void handle_event(union event_data *ev) {
     case EVENT_PUSH2_EVENT:
         w_handle_push2_event(ev->push2_event.dev,ev->push2_event.op);
         break;
-    case EVENT_RESET_LVM:
-        w_reset_lvm();
-    	break;
     case EVENT_QUIT:
         quit = true;
         break;
